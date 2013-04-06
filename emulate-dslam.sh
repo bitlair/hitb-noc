66ce04488b3ce4a08127febedfc913d225b42acb
Wilco Baan Hofman <wilco@baanhofman.nl>
2013-04-06 22:24:14 +0000

    Update the proper vlan base

--- emulate-dslam.sh
+++ emulate-dslam.sh
@@ -3,34 +3,34 @@
 # T-Mobile DSLAM emulation on 7 VLANs
 #
 
-vconfig add eth0 20
-vconfig add eth0 21
-vconfig add eth0 22
-vconfig add eth0 23
-vconfig add eth0 24
-vconfig add eth0 25
-vconfig add eth0 26
+vconfig add eth0 700
+vconfig add eth0 701
+vconfig add eth0 702
+vconfig add eth0 703
+vconfig add eth0 704
+vconfig add eth0 705
+vconfig add eth0 706
 
-ip addr add 212.64.109.1/24 dev eth0.20
-ip addr add 212.64.109.1/24 dev eth0.21
-ip addr add 212.64.110.1/24 dev eth0.22
-ip addr add 212.64.110.1/24 dev eth0.23
-ip addr add 212.64.110.1/24 dev eth0.24
-ip addr add 212.64.110.1/24 dev eth0.25
-ip addr add 212.64.110.1/24 dev eth0.26
+ip addr add 212.64.109.1/24 dev eth0.700
+ip addr add 212.64.109.1/24 dev eth0.701
+ip addr add 212.64.110.1/24 dev eth0.702
+ip addr add 212.64.110.1/24 dev eth0.703
+ip addr add 212.64.110.1/24 dev eth0.704
+ip addr add 212.64.110.1/24 dev eth0.705
+ip addr add 212.64.110.1/24 dev eth0.706
 
-ip link set eth0.20 up mtu 1492
-ip link set eth0.21 up mtu 1492
-ip link set eth0.22 up mtu 1492
-ip link set eth0.23 up mtu 1492
-ip link set eth0.24 up mtu 1492
-ip link set eth0.25 up mtu 1492
-ip link set eth0.26 up mtu 1492
+ip link set eth0.700 up mtu 1492
+ip link set eth0.701 up mtu 1492
+ip link set eth0.702 up mtu 1492
+ip link set eth0.703 up mtu 1492
+ip link set eth0.704 up mtu 1492
+ip link set eth0.705 up mtu 1492
+ip link set eth0.706 up mtu 1492
 
-ip route add 212.64.109.221 dev eth0.20
-ip route add 212.64.109.241 dev eth0.21
-ip route add 212.64.110.65 dev eth0.22
-ip route add 212.64.110.124 dev eth0.23
-ip route add 212.64.110.150 dev eth0.24
-ip route add 212.64.110.173 dev eth0.25
-ip route add 212.64.110.183 dev eth0.26
+ip route add 212.64.109.221 dev eth0.700
+ip route add 212.64.109.241 dev eth0.701
+ip route add 212.64.110.65 dev eth0.702
+ip route add 212.64.110.124 dev eth0.703
+ip route add 212.64.110.150 dev eth0.704
+ip route add 212.64.110.173 dev eth0.705
+ip route add 212.64.110.183 dev eth0.706
